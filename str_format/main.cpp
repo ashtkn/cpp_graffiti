0a39ba45b71bfbf6c178b4a0fee41fd9312705d4
Asahi Takenouchi <asahi.taken@gmail.com>
2023-01-18 23:02:29 +0900

    feat: create StrFormat function

---
+++ str_format/main.cpp
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+
+#include <iostream>
+#include <string>
+
+/**
+ * @brief 1023文字（+ヌル文字）までの文字列をフォーマットして返す
+ */
+std::string StrFormat(const char* format, ...) {
+  va_list args;
+  va_start(args, format);
+  char buffer[1024];
+  vsnprintf(buffer, sizeof(buffer), format, args);
+  va_end(args);
+  return std::string(buffer);
+}
+
+int main(int argc, char const* argv[]) {
+  std::cout << StrFormat("Hello %s", "World") << std::endl;
+  return 0;
+}
